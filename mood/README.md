02e31dc327236adb6475114608f5c441ac017d8a
Andrew Stone <andrew@clovar.com>
2012-12-15 02:06:33 -0500

    Mood: better color choices and fading + README showing all possible colors

---
+++ mood/README.md
@@ -0,0 +1,8 @@
+# A simple mood light
+
+## Schematic
+
+(Soon)
+
+## All of the colors that the light will glow
+![Possible glow colors](https://github.com/thatguystone/arduino/raw/master/mood/colors.png)
--- mood/mood.ino
+++ mood/mood.ino
@@ -2,69 +2,54 @@
 #define GREEN 10
 #define BLUE 11
 
-#define MIN_LED_VAL 5
+#define STEP 5
 
 struct light {
 	int pin;
 	int val;
-	int dir;
 };
 
 struct light lights[] = {
-	{RED, 255/3, 0},
-	{GREEN, 255/3, 0},
-	{BLUE, 255/3, 0},
+	{RED, 0},
+	{GREEN, 0},
+	{BLUE, 0},
 };
 
 #define NUM_LIGHTS (sizeof(lights) / sizeof(*lights))
 
-// The light that is currently fading up
-static int master = 0;
+// The light being faded out
+static int out = 1;
 
-static void randomize() {
-	int dir = lights[master].dir = random(2, MIN_LED_VAL+1);
-	
-	// Ensure that both non-master lights will change at the same time (min == 1)
-	int reverse_dir = random(1, dir);
-	
-	int dirs[] = { -reverse_dir, -(dir - reverse_dir) };
-	int which = 0;
-	
-	for (int i = 0; i < NUM_LIGHTS; i++) {
-		if (i != master) {
-			lights[i].dir = dirs[which++];
-		}
-	}
-}
+// The light being faded in
+static int in = 0;
 
 void setup() {
 	for (int i = 0; i < NUM_LIGHTS; i++) {
 		pinMode(lights[i].pin, OUTPUT);
 	}
-	
-	randomize();
-	
-	analogWrite(RED, 5);
-	analogWrite(GREEN, 245);
-	analogWrite(BLUE, 5);
 }
 
 void loop() {
-	int new_master = -1;
-	
-	for (int i = 0; i < NUM_LIGHTS; i++) {
-		analogWrite(lights[i].pin, lights[i].val < 0 ? 0 : lights[i].val);
-		lights[i].val += lights[i].dir;
+	while (lights[in].val < 255) {
+		lights[in].val += STEP;
 		
-		if (lights[i].val < MIN_LED_VAL) {
-			new_master = i;
+		if (lights[out].val > 0) {
+			lights[out].val -= STEP;
+		}
+		
+		for (int i = 0; i < NUM_LIGHTS; i++) {
+			analogWrite(lights[i].pin, lights[i].val);
+		}
+		
+		delay(100);
+	}
+	
+	int to = random(0, 2);
+	for (int i = 0; i < NUM_LIGHTS; i++) {
+		if (i != in && to-- == 0) {
+			out = in;
+			in = i;
+			break;
 		}
 	}
-	
-	if (new_master != -1) {
-		master = new_master;
-		randomize();
-	}
-	
-	delay(100);
 }
